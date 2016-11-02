4c9e97ae7ca4d4f393d828252143990670a58bf5
hyeonsj413 <hyeonsj413@gmail.com>
2016-11-02 16:44:48 +0900

    중첩 제거

--- 3/3.2/app.js
+++ 3/3.2/app.js
@@ -1,3 +1,40 @@
-/**
- * Created by Slogup on 2016. 11. 2..
- */
+var http = require('http');
+var fs = require('fs');
+
+
+var server = http.createServer(function(req, res){
+    getTitles(res);
+
+}).listen(8000, "127.0.0.1");
+
+
+function getTitles(res){
+
+    fs.readFile('./titles.json', function(err, data){
+        if(err) hadError(err, res);
+
+        getTemplate(JSON.parse(data.toString()), res);
+
+    })
+
+}
+
+
+function getTemplate(titles, res){
+    fs.readFile('./template.html', function(err, data){
+        if(err) hadError(err, res);
+
+        formatHtml(titles, data.toString(), res);
+    })
+}
+
+function formatHtml(titles, tmpl, res){
+    var html = tmpl.replace('%', titles.join('</li><li>'));
+    res.writeHead(200, {'content-Type': 'text/html'});
+    res.end(html);
+}
+
+function hadError(err, res){
+    console.error(err);
+    res.end('Server Error');
+}
